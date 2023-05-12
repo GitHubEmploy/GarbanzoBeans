abf68961eb9617761828f48d39a26606ee3d6e10
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2023-05-12 16:32:20 -0700

    Create arduinoshit.ino
    This is the Arduino side code that reads the ultrasonic data and feeds it to a serial stream for RPI to read

---
+++ arduinoshit.ino
@@ -0,0 +1,34 @@
+// Define pin numbers
+const int trigPin = 7;  // Pin connected to the trigger signal of the ultrasonic sensor
+const int echoPin = 6;  // Pin connected to the echo signal of the ultrasonic sensor
+
+// Declare variables
+long duration;  // Time it takes for the ultrasonic pulse to travel to the object and back
+int distance;   // Calculated distance based on the measured duration
+
+void setup() {
+  pinMode(trigPin, OUTPUT);  // Set the trigPin as an output
+  pinMode(echoPin, INPUT);   // Set the echoPin as an input
+  Serial.begin(9600);        // Start the serial communication at a baud rate of 9600
+}
+
+void loop() {
+  // Clear the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Set the trigPin to HIGH state for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the echoPin and measure the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculate the distance based on the measured duration
+  distance = duration * 0.034 / 2;
+
+  // Print the distance to the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+}
